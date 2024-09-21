fbfeea4fc3112002a5bd1c4cd8539a23d8f1c94a
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-09-21 19:04:28 +0545

    auth user

---
+++ routers/authenticate.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from .. import database, models, tokenkey
+from sqlalchemy.orm import Session
+from ..hashing import Hash
+
+router = APIRouter(tags=["Authentication"])
+
+
+@router.post("/login")
+def login(
+    request: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(database.get_db),
+):
+    user = db.query(models.User).filter(models.User.email == request.username).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials"
+        )
+
+    if not Hash.verify(user.password, request.password):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials"
+        )
+
+    # generate a JWT token and return
+    access_token = tokenkey.create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
