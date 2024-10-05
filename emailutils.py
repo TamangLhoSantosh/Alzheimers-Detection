1a531b9057ecafbeb85636b4276d9c9ba8945aaf
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-10-05 22:10:25 +0545

    email utilities

---
+++ emailutils.py
@@ -0,0 +1,28 @@
+import aiosmtplib
+from email.message import EmailMessage
+import config
+
+EMAIL_HOST = config.EMAIL_HOST
+EMAIL_PORT = config.EMAIL_PORT
+EMAIL_USER = config.EMAIL_USER
+EMAIL_PASSWORD = config.EMAIL_PASSWORD
+
+
+async def send_verification_email(email: str, token: str):
+    message = EmailMessage()
+    message["From"] = EMAIL_USER
+    message["To"] = email
+    message["Subject"] = "Email Verification"
+    verification_link = f"http://localhost:8000/verify-email?token={token}"
+    message.set_content(
+        f"Please click the following link to verify your email: {verification_link}"
+    )
+
+    await aiosmtplib.send(
+        message,
+        hostname=EMAIL_HOST,
+        port=EMAIL_PORT,
+        username=EMAIL_USER,
+        password=EMAIL_PASSWORD,
+        use_tls=True,
+    )
