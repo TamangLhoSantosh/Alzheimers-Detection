f56211b92693bebc6e11edea3ae4c10f3e17950a
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-10-06 18:54:12 +0545

    database url

--- database.py
+++ database.py
@@ -1,10 +1,14 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = (
-    "mysql+pymysql://root:Xic00000@localhost:3306/alzhimersdetection"
-)
+load_dotenv()
+
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
