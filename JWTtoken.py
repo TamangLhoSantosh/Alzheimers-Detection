86b97d2d1890eac64c6121353269965c8be77485
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-10-14 21:18:21 +0545

    refac-> improved clarity

--- JWTtoken.py
+++ JWTtoken.py
@@ -17,117 +17,106 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-credentials_exception = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Could not validate credentials",
-    headers={"WWW-Authenticate": "Bearer"},
-)
+
+def raise_credentials_exception(
+    status: str = status.HTTP_401_UNAUTHORIZED,
+    detail: str = "Could not validate credentials",
+):
+    raise HTTPException(
+        status_code=status,
+        detail=detail,
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+
+def decode_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise_credentials_exception()
+
+
+def get_user_from_email(email: str, db: Session) -> models.User:
+    user = db.query(models.User).filter(models.User.email == email).first()
+    if user is None:
+        raise_credentials_exception("User Not Found")
+    return user
 
 
 def create_access_token(
     data: dict, expires_delta: timedelta = None, refresh: bool = False
-):
-    payload = {}
-    payload["sub"] = data.get("sub")
-    payload["exp"] = datetime.now(timezone.utc) + (
+) -> str:
+    expire = (
         expires_delta
         if expires_delta is not None
         else timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     )
-    payload["jti"] = str(uuid.uuid4())
-    payload["iat"] = datetime.now(timezone.utc)
-    payload["refresh"] = refresh
 
+    payload = {
+        "sub": data.get("sub"),
+        "exp": datetime.now(timezone.utc) + expires_delta,
+        "jti": str(uuid.uuid4()),
+        "iat": datetime.now(timezone.utc),
+        "refresh": refresh,
+    }
     token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
     return token
 
 
-def verify_token(
-    token: str,
-    db: Session = Depends(database.get_db),
-):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        if payload.get("refresh"):
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid access token"
-            )
-
-        email: str = payload.get("sub")
-        if email is None:
-            raise credentials_exception
-        token_data = schemas.TokenData(email=email)
-
-        # Query the database using the provided session (db)
-        user = (
-            db.query(models.User).filter(models.User.email == token_data.email).first()
-        )
-
-        if user is None:
-            raise credentials_exception
-
-    except JWTError:
-        raise credentials_exception
-
-    return user
-
-
 def verify_user_email(token: str, db: Session):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    payload = decode_token(token)
 
-        email: str = payload.get("sub")
-        if email is None:
-            raise credentials_exception
+    email: str = payload.get("sub")
+    if email is None:
+        raise_credentials_exception()
 
-        token_data = schemas.TokenData(email=email)
+    token_data = schemas.TokenData(email=email)
 
-        user = (
-            db.query(models.User).filter(models.User.email == token_data.email).first()
-        )
-        if user is None:
-            raise credentials_exception
+    user = get_user_from_email(token_data.email, db)
 
-        if user.is_verified:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Email already verified",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
+    if user.is_verified:
+        raise_credentials_exception(status.HTTP_409_CONFLICT, "Email already verified")
 
-        user.is_verified = True
-        db.commit()
-        db.refresh(user)
-
-    except JWTError:
-        raise credentials_exception
+    user.is_verified = True
+    db.commit()
+    db.refresh(user)
 
     return "Your email is verified."
 
 
-def refresh_token(refresh_token: str, db: Session = Depends(database.get_db)):
-    try:
-        payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
-        if not payload.get("refresh"):
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token"
-            )
-
-        email: str = payload.get("sub")
-        if email is None:
-            raise credentials_exception
-
-        user = db.query(models.User).filter(models.User.email == email).first()
-        if user is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found"
-            )
-
-        # Create a new access token
-        new_access_token = create_access_token(data={"sub": user.email})
-        return {"access_token": new_access_token, "token_type": "bearer"}
-
-    except JWTError:
+def verify_access_token(
+    token: str, db: Session = Depends(database.get_db)
+) -> models.User:
+    payload = decode_token(token)
+    if payload.get("refresh"):
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid access token"
         )
+    email: str = payload.get("sub")
+    if email is None:
+        raise_credentials_exception()
+
+    user = get_user_from_email(email, db)
+
+    return user
+
+
+def verify_refresh_token(
+    refresh_token: str, db: Session = Depends(database.get_db)
+) -> dict:
+    payload = decode_token(refresh_token)
+    if not payload.get("refresh"):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token"
+        )
+
+    email: str = payload.get("sub")
+    if email is None:
+        raise_credentials_exception()
+
+    user = get_user_from_email(email, db)
+
+    # Create a new access token
+    new_access_token = create_access_token(data={"sub": user.email})
+    return {"access_token": new_access_token, "token_type": "bearer"}
